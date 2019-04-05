538cf5d759becbb6514b9cd7dd25294ec6767bfe
Pablo <gimenezpablonl@gmail.com>
2019-04-05 12:19:39 -0300

    sigin/signup function added

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+
+const User = new Schema({
+    username : String,
+    password : String,
+    email: String,
+    position : Number
+});
+
+User.methods.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  };
+  
+User.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  
+module.exports = mongoose.model('User', User);
