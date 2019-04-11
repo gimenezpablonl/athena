efd1712c005cf62b9493b1e872210fc0db36cf2d
Pablo <gimenezpablonl@gmail.com>
2019-04-10 21:04:00 -0300

    Added sex and birthday

--- src/models/Student.js
+++ src/models/Student.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Student = new Schema({
-    record_number : Number,
-    name : String,
-    last_name : String,
-	dni : String,
+    record_number:{
+        type: Number,
+        unique: true
+    },
+    name: String,
+    last_name: String,
+	dni:{
+        type: String,
+        unique: true
+    },
+    sex: Boolean,
+    birthday: Date
 });
 
 module.exports = mongoose.model('Student', Student);
