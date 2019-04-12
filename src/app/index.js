5a28cb399195bad7f3b5e51311d6b67ed472e6af
Pablo <gimenezpablonl@gmail.com>
2019-04-12 20:28:46 -0300

    Vuetify theme created

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 import Vue from 'vue';
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
@@ -8,7 +9,17 @@ import store from './store'
 import Axios from 'axios'
 
 Vue.use(Vuelidate);
-Vue.use(Vuetify);
+Vue.use(Vuetify,{
+  theme: {
+    primary: colors.deepPurple.lighten1,
+    secondary: colors.deepPurple,
+    accent: colors.shades.black,
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+});
 
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem('token')
